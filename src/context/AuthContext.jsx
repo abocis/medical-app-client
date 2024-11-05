5d4a8d6c8f4875a788188d47a7257235cc43a716
Helena <helena@klarr.se>
2024-11-05 16:34:02 +0100

    added userId to login, authContext and requireAuth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,11 +2,13 @@ import { createContext, useState } from "react";
 // authentication context to handle global auth
 export const AuthContext = createContext();
 
+// Helena: la till userId här
 export const AuthProvider = ({ children }) => {
   const [authState, setAuthState] = useState({
     isAuthenticated: false,
     user: null,
     roles: [],
+    userId: "",
   });
 
   return (
